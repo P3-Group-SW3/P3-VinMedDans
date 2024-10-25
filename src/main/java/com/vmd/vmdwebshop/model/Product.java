74800df18e13c18cefe0dcbf568fe64ff7ab7db4
Nikomiko <ngool23@student.aau.dk>
2024-10-25 14:37:14 +0200

    Update Wine and Product classes

--- src/main/java/com/vmd/vmdwebshop/model/Product.java
+++ src/main/java/com/vmd/vmdwebshop/model/Product.java
@@ -1,9 +1,56 @@
 package com.vmd.vmdwebshop.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Setter;
+
+@MappedSuperclass
 public abstract class Product {
+    @Id @Column(name="")
     private String ID;
+    @Column(name="")
     private int price;
+    @Column(name="")
     private String description;
+    @Column(name="")
     private String imageURL;
+    @Column(name="")
     private int discount;
+
+    public Product() {}
+
+    public Product(String ID, int price, String description, String imageURL, int discount) {
+        this.ID = ID;
+        this.price = price;
+        this.description = description;
+        this.imageURL = imageURL;
+        this.discount = discount;
+    }
+
+    public int getDiscount() { return this.discount; }
+
+    public String getDescription() { return this.description; }
+
+    public String getImageURL() { return this.imageURL; }
+
+    public int getPrice() { return this.price; }
+
+    public String getID() { return this.ID; }
+
+    abstract void setAmountLeft(int amountLeft);
+
+    abstract int getAmountLeft();
+
+    void setPrice(int price) {
+        this.price = price;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
 }
--- src/main/java/com/vmd/vmdwebshop/model/Wine.java
+++ src/main/java/com/vmd/vmdwebshop/model/Wine.java
@@ -1,6 +1,43 @@
 package com.vmd.vmdwebshop.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+import org.springframework.data.annotation.Id;
+
+
+@Entity
+@Table(name="Wine")
 public class Wine extends Product{
+
+    @Column(name="name")
     private String name;
+
+    @Column(name="amountLeft")
     private int amountLeft;
+
+    public Wine(){}
+
+    public Wine(String ID, int price, String description, String imageURL, int discount, String name, int amountLeft) {
+        super(ID, price, description, imageURL, discount);
+        this.name = name;
+        this.amountLeft = amountLeft;
+    }
+
+    String getName() { return this.name; }
+
+    int getAmountLeft() { return this.amountLeft; }
+
+    public boolean canBePurchased(int amountPurchased){
+        return amountLeft - amountPurchased >= 0;
+    }
+
+    @Override
+    void setAmountLeft(int amountLeft) { this.amountLeft = amountLeft; }
+
+    //MÅske er denne unødvendig
+    void addToAmountLeft(int amountPurchased){ this.amountLeft += amountPurchased; }
 }
