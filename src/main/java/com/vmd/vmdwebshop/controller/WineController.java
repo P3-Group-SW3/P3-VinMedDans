eacb078b458b43a9511bfc533da583f7dcad1b51
ceci000 <SK22VJ@student.aau.dk>
2024-10-28 10:56:28 +0100

    database repository setup

---
+++ src/main/java/com/vmd/vmdwebshop/controller/WineController.java
@@ -0,0 +1,27 @@
+//package com.vmd.vmdwebshop.controller;
+//
+//import com.vmd.vmdwebshop.model.Wine;
+//import com.vmd.vmdwebshop.service.WineService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class WineController {
+//
+//    private final WineService wineService;
+//
+//    @Autowired
+//    public WineController(WineService wineService) {
+//        this.wineService = wineService;
+//    }
+//
+//    @RequestMapping(value="/webshop", method= RequestMethod.GET)
+//    public ResponseEntity<List<Wine>> getAll{
+//        List<Wine>
+//
+//        wineService.getAllWines();
+//    }
+//}
