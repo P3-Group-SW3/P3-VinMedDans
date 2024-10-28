30062a8bec2f9254da8986b77d6014c0479fdf46
Nikomiko <ngool23@student.aau.dk>
2024-10-28 14:55:02 +0100

    Update Cart Basics

--- src/main/java/com/vmd/vmdwebshop/model/Cart.java
+++ src/main/java/com/vmd/vmdwebshop/model/Cart.java
@@ -1,6 +1,25 @@
 package com.vmd.vmdwebshop.model;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Cart {
-    private ArrayList<OrderLine> items;
+    private ArrayList<OrderLine> orderLines;
+    private String customerID;
+
+    public Cart() {}
+    public Cart(ArrayList<OrderLine> orderLines, Customer customer) {
+        this.orderLines = orderLines;
+        //this.customerID = customer.getID();
+    }
+
+    public void setOrderLines(int index) {
+        this.orderLines.remove(index);
+    }
+
+    public double calculateTotalPrice() {
+        double total = 0;
+        for (OrderLine orderLine : orderLines) {
+            //total += orderLine.calculateOrderLine();
+        }
+        return total;
+    }
 }
