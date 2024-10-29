4c44445e12e22b992c8f4f9f0983086224b1ace8
Nikomiko <ngool23@student.aau.dk>
2024-10-29 12:28:10 +0100

    Små rettelser i cart

--- src/main/java/com/vmd/vmdwebshop/model/Cart.java
+++ src/main/java/com/vmd/vmdwebshop/model/Cart.java
@@ -2,23 +2,23 @@ package com.vmd.vmdwebshop.model;
 import java.util.*;
 
 public class Cart {
-    private ArrayList<OrderLine> orderLines;
-    private String customerID;
+    private ArrayList<OrderLine> items;
+    private Long customerID;
 
     public Cart() {}
-    public Cart(ArrayList<OrderLine> orderLines, Customer customer) {
-        this.orderLines = orderLines;
+    public Cart(ArrayList<OrderLine> items, Customer customer) {
+        this.items = items;
         //this.customerID = customer.getID();
     }
 
-    public void setOrderLines(int index) {
-        this.orderLines.remove(index);
+    public void removeOrderLine(int index) {
+        this.items.remove(index);
     }
 
     public double calculateTotalPrice() {
         double total = 0;
-        for (OrderLine orderLine : orderLines) {
-            //total += orderLine.calculateOrderLine();
+        for (OrderLine orderLine : items) {
+            total += orderLine.calculateOrderLine();
         }
         return total;
     }
