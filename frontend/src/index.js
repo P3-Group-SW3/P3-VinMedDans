93c45064b3c7f087b5602346284a074754d28967
Emil Andersen <minnyecomputer.com@gmail.com>
2024-10-30 13:15:35 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Checkout from "./pages/Checkout";
-import Landingpage from "./pages/Landingpage";
+import Landingpage from "./pages/LandingPage";
 import ProductPage from "./pages/ProductPage";
 
 const SetTitle = () => {
