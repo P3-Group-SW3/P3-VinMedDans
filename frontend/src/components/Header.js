95e612fbee485e35de333d33d927b55f9d4b5726
Alberte Lohse <AF44KT@student.aau.dk>
2024-10-31 12:41:57 +0100

    Modal test for Felix
    Example modal inserted into the program (does not work, Felix will fix it)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Component } from 'react';
 import logoImg from '../images/Logo Img.png';
 import logoText from '../images/Logo Text.png';
+import CartOverlay from './CartOverlay'
 
 const Header = () => {
   useEffect(() => {
@@ -11,11 +12,20 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      <img src={logoImg} alt="Logo" className="me-2" />
-      <img src={logoText} alt="Secondary Logo" />
-    </div>
-  );
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <img src={logoImg} alt="Logo" className="me-2"/>
+        <img src={logoText} alt="Secondary Logo"/>
+        <div>
+            <CartOverlay show={this.state.show} handleClose={this.hideModal}>
+                <p>Modal</p>
+            </CartOverlay>
+            <button type="button" onClick={this.showModal}>
+                Open
+            </button>
+        </div>
+      </div>
+  )
+      ;
 };
 
 export default Header;
