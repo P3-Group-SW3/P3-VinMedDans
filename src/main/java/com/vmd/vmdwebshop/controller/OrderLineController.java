7922e39ce37b58b37f39db287f90faaab6f771b9
Nikomiko <ngool23@student.aau.dk>
2024-11-01 11:14:36 +0100

    ResponseEntity
    Har tilføjet responseentity til controlleren

--- src/main/java/com/vmd/vmdwebshop/controller/OrderLineController.java
+++ src/main/java/com/vmd/vmdwebshop/controller/OrderLineController.java
@@ -4,6 +4,7 @@ package com.vmd.vmdwebshop.controller;
 import com.vmd.vmdwebshop.repository.OrderLineRepository;
 import com.vmd.vmdwebshop.repository.WineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.vmd.vmdwebshop.service.OrderLineService;
 import com.vmd.vmdwebshop.model.*;
@@ -23,7 +24,7 @@ public class OrderLineController {
     private WineRepository wineRepository;
 
     @PostMapping("/api/createOrderLine")
-    public List<OrderLine> createOrderLine(@RequestBody Long customer_ID, int amount, String wine_ID){
+    public ResponseEntity<List<OrderLine>> createOrderLine(@RequestBody Long customer_ID, int amount, String wine_ID){
 
         OrderLine orderLine = orderLineService.createOrderLine(customer_ID, amount, Long.parseLong(wine_ID));
         Wine wine = wineRepository.findById(Long.parseLong(wine_ID)).get();
@@ -31,32 +32,31 @@ public class OrderLineController {
 
         double orderLinePrice = orderLineService.calculateOrderLine(orderLine.getAmount(), wine.getPrice());
 
-        return orderLines;
+        return ResponseEntity.ok(orderLines);
     }
 
     @PostMapping("/api/editOrderLine")
-    public void editOrderLine(@RequestBody Long customer_ID, int amount, String wine_ID){
-
-
+    public ResponseEntity<List<OrderLine>> editOrderLine(@RequestBody Long customer_ID, int amount, String wine_ID){
 
         orderLineService.editOrderLine(customer_ID, amount, Long.parseLong(wine_ID));
+
+        return ResponseEntity.ok(orderLineRepository.findByCustomerId(customer_ID));
     }
     //Funktion der tager ID, amoung, WineID.
     //Først finder vi den specifikke orderline på baggrund af customerID først, og bagefter WineID,
     //Så skal vi ændre i amountet på orderlinen
 
 
-    @PostMapping("/api/clearCart")
-    public void clearCart(@RequestBody String customer_ID){
+    @DeleteMapping("/api/clearCart/{id}")
+    public void clearCart(@PathVariable String customer_ID){
         orderLineService.clearCart(Long.parseLong(customer_ID));
     }
 
     @PostMapping("/api/incrementOrderLine")
-    public List<OrderLine> modulateOrderLine(@RequestBody String customer_ID, String wine_ID, boolean increment){
+    public ResponseEntity<List<OrderLine>> modulateOrderLine(@RequestBody String customer_ID, String wine_ID, boolean increment){
 
 
-
-        return orderLineRepository.findByCustomerId(Long.parseLong(customer_ID));
+        return ResponseEntity.ok(orderLineRepository.findByCustomerId(Long.parseLong(customer_ID)));
 
     }
 
--- src/main/java/com/vmd/vmdwebshop/repository/OrderLineRepository.java
+++ src/main/java/com/vmd/vmdwebshop/repository/OrderLineRepository.java
@@ -12,7 +12,6 @@ public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
     @Query(value="SELECT * FROM OrderLine WHERE customer_ID = ?1", nativeQuery = true)
     List<OrderLine> findByCustomerId(Long customer_ID);
 
-
     @Modifying
     @Query(value="DELETE FROM OrderLine WHERE customer_ID = ?1", nativeQuery = true)
     void deleteOrderLinesByCustomerId(Long customer_ID);
