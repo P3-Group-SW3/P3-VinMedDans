9960bef9976ef8e3c5ad9915d742b903344ecad6
Lenovo <minnyecomputer.com@gmail.com>
2024-11-04 12:24:38 +0100

    Changed unique imagename code

--- src/main/java/com/vmd/vmdwebshop/service/imageService.java
+++ src/main/java/com/vmd/vmdwebshop/service/imageService.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Random;
 
 @Service
 public class imageService {
@@ -22,21 +23,15 @@ public class imageService {
             Files.createDirectories(uploadPath);
         }
 
-        String fileName = file.getOriginalFilename();
-        if (fileName == null || fileName.isEmpty()) {
-            fileName = "image.png"; // Default filename
-        } else {
-            String fileExtension = fileName.substring(fileName.lastIndexOf('.'));
-            String uniqueIdentifier;
-            Path filePath;
-            do {
-                uniqueIdentifier = System.currentTimeMillis() + "_" + (int)(Math.random() * 1000);
-                fileName = uniqueIdentifier + fileExtension;
-                filePath = uploadPath.resolve(fileName);
-            } while (Files.exists(filePath));
-        }
+        String fileName;
+        Path filePath;
+        Random random = new Random();
+        do {
+            int randomNumber = 10000 + random.nextInt(90000); 
+            fileName = "image_" + randomNumber + ".png";
+            filePath = uploadPath.resolve(fileName);
+        } while (Files.exists(filePath));
 
-        Path filePath = uploadPath.resolve(fileName);
         Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
 
         return filePath.toString();
