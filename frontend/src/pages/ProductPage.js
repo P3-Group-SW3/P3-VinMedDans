bac2922e3be1f8187551a5195f071a999f27a502
Lenovo <minnyecomputer.com@gmail.com>
2024-11-07 10:02:30 +0100

    More images in image folder

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Header from "../components/Header";
-import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Item from "../components/Item";
 
