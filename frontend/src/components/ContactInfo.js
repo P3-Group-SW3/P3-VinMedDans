eaa51643f4165fc19ebfc173e195b8f40e3f0624
Alberte Lohse <AF44KT@student.aau.dk>
2024-11-11 11:36:45 +0100

    Checkout button updated
    Button is formatted as other buttons and form is now connected to backend (in theory)

--- frontend/src/components/ContactInfo.js
+++ frontend/src/components/ContactInfo.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles/button.css'
 
 
 function ContactInfo() {
     return (
         <section>
             <h2>Kontaktoplysninger</h2>
-            <form method="POST">
+            <form method="POST" action="/api/orderInfo">
                 <div className="form-group">
                     <input type="text" className="form-control mt-2" placeholder="Fornavn"/>
                     <input type="text" className="form-control mt-2" placeholder="Efternavn"/>
@@ -18,7 +19,7 @@ function ContactInfo() {
                     <input type="text" className="form-control mt-2" placeholder="Postnummer"/>
                     <input type="text" className="form-control mt-2" placeholder="By"/>
                 </div>
-                <button type="submit" className="btn btn-dark mt-3">Bekræft Køb</button>
+                <button type="submit" className="button mt-3">Bekræft Køb</button>
             </form>
         </section>
     );
