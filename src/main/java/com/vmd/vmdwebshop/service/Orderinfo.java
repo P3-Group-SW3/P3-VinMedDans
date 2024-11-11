13fdc8c34258f9d899365aafe1049a9ffdc3032f
CelestrialDragon <gustav.tue@gmail.com>
2024-11-11 14:58:56 +0100

    cleanup

--- src/main/java/com/vmd/vmdwebshop/service/Orderinfo.java
+++ src/main/java/com/vmd/vmdwebshop/service/Orderinfo.java
@@ -1,18 +1,33 @@
 package com.vmd.vmdwebshop.service;
 import com.vmd.vmdwebshop.model.*;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
 public class Orderinfo {
+
+    @NotBlank(message = "Dette felt må ikke være blankt")
     private String firstName;
+    @NotBlank(message = "Dette felt må ikke være blankt")
     private String lastName;
+    @NotBlank(message = "Dette felt må ikke være blankt")
+    @Email(message = "Email er krævet")
     private String email;
+    @Pattern(regexp = "^(\\d{8}|\\+\\d{1,3}\\d{8,15})$", message = "Tillefon nummeret skal inten være 8 tal eller starte med land code.")
     private String phone;
+    @NotBlank(message = "Dette felt må ikke være blankt")
     private String address;
+    @NotBlank(message = "Dette felt må ikke være blankt")
+    
     private String zipCode;
+    @NotBlank(message = "Dette felt må ikke være blankt")
     private String city;
 
     /**
      * Creates an Order with it's information
-     * @return
+     * @return Orders
      */
     public Orders createOrderFromInfo() {
         Orders order = new Orders(firstName, lastName, email, phone, address, zipCode, city);
