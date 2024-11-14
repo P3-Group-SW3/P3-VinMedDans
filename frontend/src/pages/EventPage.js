04d906abfcd3a1a7b683eef7e934c0800ca7bbcc
Lenovo <minnyecomputer.com@gmail.com>
2024-11-14 13:53:39 +0100

    deleted deprecated files

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -1,9 +1,8 @@
 import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Event from "../components/Event";
 import React from "react";
-import images from "../images/images";
+
 
 function EventPage() {
 
