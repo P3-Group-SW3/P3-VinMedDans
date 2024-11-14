b8808ee44fdbbb1418ab3da6c1271c0e98d8e3c2
Lenovo <minnyecomputer.com@gmail.com>
2024-11-14 11:09:51 +0100

    WebSecurityConfig.java is configured correctly has 3 test users

---
+++ src/main/java/com/vmd/vmdwebshop/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.vmd.vmdwebshop.config;
+
+import com.vmd.vmdwebshop.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    /**
+     * @param http
+     * @return SecurityFilterChain
+     * @throws Exception
+     * Configure security filter chain
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/administrator/**", "/api/*/admin/**").authenticated()
+                    .anyRequest().permitAll())
+                .formLogin(withDefaults())
+                .logout(withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
