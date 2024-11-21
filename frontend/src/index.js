b8e219ceb877894b4b6593b5032a23a9e865ef0f
Lenovo <minnyecomputer.com@gmail.com>
2024-11-21 02:55:51 +0100

    first try

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Checkout from "./pages/Checkout";
-import Landingpage from "./pages/LandingPage";
-import ProductPage from "./pages/ProductPage";
-import AboutUsPage from "./pages/AboutUsPage";
-import LocationPage from "./pages/LocationPage";
-import ContactPage from "./pages/ContactPage";
-import EventPage from "./pages/EventPage";
-import LoadFonts from "./components/LoadFonts";
+import Checkout from './pages/Checkout';
+import Landingpage from './pages/LandingPage';
+import ProductPage from './pages/ProductPage';
+import AboutUsPage from './pages/AboutUsPage';
+import LocationPage from './pages/LocationPage';
+import ContactPage from './pages/ContactPage';
+import EventPage from './pages/EventPage';
+import LoadFonts from './components/LoadFonts';
+import CriiptoAuth from './components/CriiptoAuthWrapper';
+import Callback from './components/Callback';
 
 const SetTitle = () => {
   useEffect(() => {
@@ -29,12 +31,17 @@ root.render(
       <Routes>
         <Route path="/">
           <Route index element={<Landingpage />} />
-          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout" element={
+            <CriiptoAuth>
+              <Checkout />
+            </CriiptoAuth>
+          } />
           <Route path="shop" element={<ProductPage />} />
           <Route path="about" element={<AboutUsPage />} />
           <Route path="locations" element={<LocationPage />} />
           <Route path="contact" element={<ContactPage />} />
           <Route path="events" element={<EventPage />} />
+          <Route path="callback" element={<Callback />} />
           <Route path="*" element={<h1>Page not found</h1>} />
         </Route>
       </Routes>
