58f9c0128ef2accde420be2f4bf5b4082443934b
wefjyl <148341521+wefjyl@users.noreply.github.com>
2024-11-22 13:55:36 +0100

    Update ProductsAdmin.js
    Fjernede dobbel fetch

--- frontend/src/components/ProductsAdmin.js
+++ frontend/src/components/ProductsAdmin.js
@@ -11,8 +11,6 @@ function ProductsAdmin() {
 
         fetch('/api/wine/getList')  // Opdateret endpoint
 
-        fetch('/api/wine/getList')
-
             .then(response => {
                 if (!response.ok) throw new Error('Network response was not ok');
                 return response.json();
