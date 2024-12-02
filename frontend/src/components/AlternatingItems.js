c7efcb64c559e25f812ee0d73cde20f2af8bee78
Alberte Lohse <AF44KT@student.aau.dk>
2024-12-02 18:01:43 +0100

    ProductPage updated
    Now adheres to the alternating layout that the landing page uses

--- frontend/src/components/AlternatingItems.js
+++ frontend/src/components/AlternatingItems.js
@@ -1,28 +1,36 @@
 import React from "react";
 import Button from "./Button";
+import CartModify from "./CartModify"
 import { useNavigate } from "react-router-dom";
 
 const AlternatingItems = ({ items }) => {
 
     const navigate = useNavigate();
+    console.log('Items:',items);
 
     return (
         <div className="container my-5">
             {items.map((item, index) => (
-                <div className="row align-items-center mb-4" key={index}>
-                    <div className={`col-lg-6 ${index % 2 === 0 ? "order-lg-2" : "order-lg-1"}`}>
+                <div className="row align-items-center mb-4" key={index} style={{backgroundColor: item.title ? "#FFFFFF" : "#FFFAED"}}>
+                    <div className={`col-lg-6 ${index % 2 === 0 ? "order-lg-2" : "order-lg-1"} py-0`}>
                         <img
-                            src={item.imagePath}
-                            alt={item.title}
+                            src={item.imageURL}
+                            alt={item.title? item.title : item.name}
                             className="img-fluid mx-auto d-block"
                         />
                     </div>
                     <div className={`col-lg-6 ${index % 2 === 0 ? "order-lg-1" : "order-lg-2"} text-center text-lg-start`}>
-                        <h3 className="header-large pt-3">{item.title}</h3>
+                        <h3 className="header-large pt-3">{item.title ? item.title : item.name}</h3>
+                        {item.price && <h2 className="price-text" > {item.price} DKK </h2>}
                         <p className="body-text">{item.description}</p>
-                        <div className="d-flex justify-content-center pt-3">
-                            {item.buttonText && <Button text={item.buttonText} isWide={true} onClick={() => navigate(item.buttonPath)}/> }
+                        {item.buttonText && <div className="d-flex justify-content-center pt-3">
+                            <Button text={item.buttonText} isWide={true} onClick={() => navigate(item.buttonPath)}/>
                         </div>
+                        }
+                        {item.amountLeft && <div className="d-flex pt-3">
+                            < CartModify item={item} />
+                        </div>
+                        }
                     </div>
                 </div>
             ))}
--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Item from "../components/Item";
 import {CustomerLinks} from "./CustomerLinkContext";
+import AlternatingItems from "../components/AlternatingItems";
 
 function ProductPage() {
     const [items, setItems] = useState([]);
@@ -28,19 +29,8 @@ function ProductPage() {
                 links={ CustomerLinks }
                 showCart={true}
             />
-            
-            {items.map((item, index) => (
-                <Item
-                    key={index}
-                    title={item?.name}
-                    description={item?.description}
-                    price={item?.price?.toString()} 
-                    image={item.imageURL}
-                    stock={item?.stock?.toString()}
-                    imagePos="left"
-                    item={item}
-                ></Item>
-            ))}
+
+            <AlternatingItems items={items}/>
             <Footer />
         </div>
     );
