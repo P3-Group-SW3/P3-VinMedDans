6e6b9d98b93a3c4a77532dd1dbe02f5729dd10cc
FelixBjerre <gk30jf@student.aau.dk>
2024-12-16 13:34:24 +0100

    Tried to fix distributorPage

---
+++ frontend/src/components/Distributor.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Distributor = ({ title, name, location, websiteURL }) => {
+
+    return (
+        <div className="container my-3" style={{ backgroundColor: '#f8f6e8'}}>
+            <div className="row no-gutters align-items-stretch">
+                <div className="col-md-6">
+                    <h2 style={{ fontFamily: 'Rubik Dirt', fontWeight: 'bold', color: '#405071' }}>{title}</h2>
+                    <p style={{ fontFamily: 'Rubik', color: 'black' }}> {name} </p>
+                    <p style={{ fontFamily: 'Rubik', color: 'black' }}> {location} </p>
+                    <p style={{ fontFamily: 'Rubik', color: 'black' }}> {websiteURL} </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+Distributor.propTypes = {
+    title: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    websiteURL: PropTypes.string.isRequired,
+};
+
+export default Distributor;
--- frontend/src/pages/LocationPage.js
+++ frontend/src/pages/LocationPage.js
@@ -2,19 +2,41 @@ import AgeVerification from "../components/AgeVerification";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import {CustomerLinks} from "./CustomerLinkContext";
+import Event from "../components/Event";
+import React from "react";
+import Distributor from "../components/Distributor";
 
 
 function LocationPage() {
-    return (
-        <div>
-            <AgeVerification/>
-            <Header
-                links={ CustomerLinks }
-                showCart={true}
-            />
-            <Footer/>
-        </div>
-    );
-}
+    const [distributors, setDistributors] = React.useState([]);
 
-export default LocationPage;
+    React.useEffect(() => {
+        fetch("/api/distributor/getList")
+            .then((response) => response.json())
+            .then((data) => setDistributors(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    function LocationPage() {
+        return (
+            <div>
+                <AgeVerification/>
+                <Header
+                    links={CustomerLinks}
+                    showCart={true}
+                />
+                {distributors.map((event) => (
+                    <Distributor
+                        key={event.id}
+                        title={distributors.title}
+                        name={distributors.name}
+                        location={distributors.locations}
+                        websiteURL={distributors.websiteURL}
+                    />
+                ))}
+                <Footer/>
+            </div>
+        );
+    }
+}
+    export default LocationPage;
